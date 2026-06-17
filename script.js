2d5987795c757035670209482e93f56f4893baa0
Anshupriyakumari <anshupriyakumari70041@gmail.com>
2026-06-17 22:58:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,18 @@
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-
-function saveTasks() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+function addTask(){
+    const newTask = document.createElement('li')
+    const taskList = document.getElementById('taskList')
+    taskList.appendChild(newTask)
+    newTask.textContent = document.getElementById('inputTask').value
+    document.getElementById('inputTask').value = ""
+    deleteTask(newTask)
 }
 
-function renderTasks() {
-    let taskList = document.getElementById("taskList");
-    taskList.innerHTML = "";
-
-    tasks.forEach((task, index) => {
-        let li = document.createElement("li");
-
-        li.innerHTML = `
-            <span class="${task.completed ? 'completed' : ''}"
-            onclick="toggleTask(${index})">
-            ${task.text}
-            </span>
-
-            <button onclick="deleteTask(${index})">❌</button>
-        `;
-
-        taskList.appendChild(li);
-    });
+function deleteTask(newTask)
+{
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent = "Delete"
+    newTask.appendChild(deleteBtn)
+    deleteBtn.onclick = function(){
+        newTask.remove()
+    }
 }
-
-function addTask() {
-    let input = document.getElementById("taskInput");
-
-    if (input.value.trim() === "") return;
-
-    tasks.push({
-        text: input.value,
-        completed: false
-    });
-
-    saveTasks();
-    renderTasks();
-    input.value = "";
-}
-
-function toggleTask(index) {
-    tasks[index].completed = !tasks[index].completed;
-    saveTasks();
-    renderTasks();
-}
-
-function deleteTask(index) {
-    tasks.splice(index, 1);
-    saveTasks();
-    renderTasks();
-}
-
-renderTasks();
